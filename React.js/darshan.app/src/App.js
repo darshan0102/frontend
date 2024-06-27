b29fc77abde2affb36f00fe0f27a033e4e47b86b
darshan <darshanchauhan5064@gmail.com>
2024-06-27 12:10:07 +0530

    27 june

---
+++ React.js/darshan.app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline text-red-600 m-44 p-80">
+    Hello world!
+  </h1>
+  </>
+  );
+}
+
+export default App;
