8b982272e435564dccc8b4df85456e649b0ae1c1
darshan <darshanchauhan5064@gmail.com>
2024-07-04 11:51:27 +0530

    4 july

---
+++ React.js/lecture-4/myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import images from './images/image1.jpg';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={images} alt="" />
+    </div>
+  );
+}
+
+export default App;
