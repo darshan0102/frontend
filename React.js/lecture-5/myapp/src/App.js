1cb5ec7039e2aa801e23b375b5a989145dc77dc3
darshan <darshanchauhan5064@gmail.com>
2024-07-09 10:41:11 +0530

    9 july

--- React.js/lecture-5/myapp/src/App.js
+++ React.js/lecture-5/myapp/src/App.js
@@ -3,6 +3,10 @@ import Card from "./components/Cart"
 import image1 from "./image/image1.jpg";
 import image2 from "./image/image2.jpg";
 import image3 from "./image/image3.jpg";
+import image4 from "./image/image4.jpg";
+import image5 from "./image/image5.jpg";
+import image6 from "./image/image6.jpg";
+
 
 function App() {
   return (
@@ -18,9 +22,13 @@ function App() {
         <Card image={image1} delPrice="6999" curPrice="5999" />
         <Card image={image2} delPrice="5999" curPrice="4999" />
         <Card image={image3} delPrice="4999" curPrice="3999" />
+        <Card image={image4} delPrice="4999" curPrice="3999" />
+        <Card image={image5} delPrice="4999" curPrice="3999" />
+        <Card image={image6} delPrice="4999" curPrice="3999" />
       </div>
     </div>
   );
 }
 
 export default App;
+ 
