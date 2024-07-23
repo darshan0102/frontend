41ee092e184c8ff36d36f17c4cf5fac4b19781b3
darshan <darshanchauhan5064@gmail.com>
2024-07-23 10:22:35 +0530

    23 july

---
+++ React.js/FilteringData/my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Filtering from './components/Filtering'
+
+function App() {
+  return (
+  
+<div>
+      <Filtering />
+    </div>
+  )
+}
+
+export default App
