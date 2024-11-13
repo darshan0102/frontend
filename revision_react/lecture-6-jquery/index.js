ef1fc1a13b7d94fd103668fdd479ed60b980e631
darshan <darshanchauhan5064@gmail.com>
2024-11-13 10:46:06 +0530

    13 nov

---
+++ revision_react/lecture-6-jquery/index.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//     $('#animateButton').click(function() {
+//       $(this).animate({
+//         width: '200px',  // Target width
+//         height: '40px'   // Target height
+//       }, 1000); // Animation duration in milliseconds (1000ms = 1s)
+//     });
+//   });
